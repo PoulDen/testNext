21121202a5338ab8218eb41db7710bcf7ba8edf7
PoulDen <noreply@github.com>
2021-11-30 12:51:18 +0200

    refactored env

--- apollo-client.js
+++ apollo-client.js
@@ -7,10 +7,10 @@ import {
 
 const client = new ApolloClient({
   link: new HttpLink({
-    uri: process.env.URL_GRAPH,
+    uri: process.env.NEXT_PUBLIC_URL_GRAPH,
     headers: {
-      "x-hasura-admin-secret": process.env.ADMIN_SECRET,
-      "x-request-id": process.env.REQUEST_ID,
+      "x-hasura-admin-secret": process.env.NEXT_PUBLIC_ADMIN_SECRET,
+      "x-request-id": process.env.NEXT_PUBLIC_REQUEST_ID,
     },
   }),
   cache: new InMemoryCache(),
